d88987a5a0e96f5fbe87195ef83d4e269592bd58
taylorferran <tailzpk@hotmail.co.uk>
2023-11-22 13:37:46 +0000

    Finish demo

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,17 @@ import React from 'react';
 import { PrimeSdk } from '@etherspot/prime-sdk';
 import { ethers } from 'ethers'
 import './App.css';
-import logo from "./etherspot.png"
-import logo2 from "./etherspotxflare.png"
+import logo from "./etherspotxflare.png"
 
 
 const App = () => {
   
   const [etherspotWalletAddress, setEtherspotWalletAddress] = React.useState('0x0000000000000000000000000000000000000000');
   const [eoaWalletAddress, setEoaWalletAddress] = React.useState('0x0000000000000000000000000000000000000000');
-  // random private key
   const [eoaPrivateKey, setEoaPrivateKey] = React.useState('0xafdfd9c3d2095ef696594f6cedcae59e72dcd697e2a7521b1578140422a4f890');
   const [destinationAddress, setDestinationAddress] = React.useState('');
   const [amount, setAmount] = React.useState('');
   const primeSdkDefault = new PrimeSdk({ privateKey: eoaPrivateKey}, { chainId: 114, projectKey: '' });
-  const [primeSDK, setPrimeSDK] = React.useState<PrimeSdk>(primeSdkDefault);
 
   const generateRandomEOA = async () => {
     // Create random EOA wallet
@@ -29,7 +26,6 @@ const App = () => {
   const generateEtherspotWallet = async () => {
     // Initialise Etherspot SDK
     const primeSdk = new PrimeSdk({ privateKey: eoaPrivateKey}, { chainId: 114, projectKey: '' });
-    setPrimeSDK(primeSDK);
     const address: string = await primeSdk.getCounterFactualAddress();
     setEtherspotWalletAddress(address);
     console.log('\x1b[33m%s\x1b[0m', `EtherspotWallet address: ${address}`);
@@ -37,26 +33,28 @@ const App = () => {
 
   const sendFunds = async () => {
 
+    const primeSdk = new PrimeSdk({ privateKey: eoaPrivateKey}, { chainId: 114, projectKey: '' });
     // clear the transaction batch
-    await primeSDK.clearUserOpsFromBatch();
+    await  primeSdk.clearUserOpsFromBatch();
 
     // add transactions to the batch
-    const transactionBatch = await primeSDK.addUserOpsToBatch({to: destinationAddress, value: ethers.utils.parseEther(amount)});
+    const transactionBatch = await primeSdk.addUserOpsToBatch({to: destinationAddress, value: ethers.utils.parseEther(amount)});
     console.log('transactions: ', transactionBatch);
 
       // estimate transactions added to the batch and get the fee data for the UserOp
-    const op = await primeSDK.estimate();
+    const op = await primeSdk.estimate();
     //console.log(`Estimate UserOp: ${await printOp(op)}`);
 
     // sign the UserOp and sending to the bundler...
-    const uoHash = await primeSDK.send(op);
+    const uoHash = await primeSdk.send(op);
     console.log(`UserOpHash: ${uoHash}`);
+    alert("Transaction sent!");
   }
 
   return (
     <div className="App-header">
 
-      <img className="App-logo" src={logo2}></img>
+      <img className="App-logo" src={logo}></img>
       <h1  className="App-title">Getting started with Etherspot Prime</h1>
 
       <p> To initialise the SDK, it requires a Key Based Wallet(KBW) to be passed in.</p>
@@ -82,26 +80,26 @@ const App = () => {
         </a>
       </p>
 
-      <a className="App-link" target="_blank" href="https://coston2-faucet.towolabs.com/">
+      <a className="App-button" target="_blank" href="https://coston2-faucet.towolabs.com/">
         Now you have a wallet created on Coston2 you can fund it with this faucet.</a>
 
-      <p>  
-        <a className="App-link" target="_blank" href="https://etherspot.fyi/prime-sdk/intro">
-        Now you have a wallet created you can explore what else we can do with the Prime SDK.</a>
+      <p>
+        Now let's try sending a transaction.
       </p>
-
-      <input
+      <input className="App-input"
               type="text"
               value={destinationAddress}
               onChange={(event) => setDestinationAddress(event.target.value)}
+              placeholder="Destination Address"
             />
-            <input
+            <input className="App-input"
               type="text"
               value={amount}
               onChange={(event) => setAmount(event.target.value)}
+              placeholder="Amount in FLR"
             />
             <hr />
-            <button onClick={() => sendFunds()}>Send</button>
+            <button className="App-button" onClick={() => sendFunds()}>Send FLR!</button>
       
     </div>
   )
